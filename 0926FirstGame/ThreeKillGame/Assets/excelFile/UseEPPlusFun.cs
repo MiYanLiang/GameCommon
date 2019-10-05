49a4da8efde1a0396587b474457026fb6e9a3446
nan <895610232@qq.com>
2019-10-05 12:48:55 +0800

    最新版本提交

--- 0926FirstGame/ThreeKillGame/Assets/excelFile/UseEPPlusFun.cs
+++ 0926FirstGame/ThreeKillGame/Assets/excelFile/UseEPPlusFun.cs
@@ -20,8 +20,8 @@ public class UseEPPlusFun : MonoBehaviour {
     /// </summary>
     private static void GetExcelFile()
     {
-        //string filePath = "D:/测试实例文件/ExcelFiles/游戏数据.xlsx";   //绝对路径
-        string filePath = Application.streamingAssetsPath + "\\TablFiles\\表格测试数据.xlsx"; //相对路径，文件从StreamingAssets文件夹中获取（file://）
+        string filePath = "F:/dev/111.xlsx";   //绝对路径
+        //string filePath = Application.streamingAssetsPath + "\\TablFiles\\表格测试数据.xlsx"; //相对路径，文件从StreamingAssets文件夹中获取（file://）
 
         //根据文件路径，获取excel文件信息
         FileInfo fileinfo = new FileInfo(filePath);
@@ -33,14 +33,69 @@ public class UseEPPlusFun : MonoBehaviour {
 
             //取得第一行第一列的数值
             //string str = worksheet.Cells[1, 1].Value.ToString();
+            //print(worksheet.Cells.Rows);
 
-            for (int i = 1; i < worksheet.Cells.Rows + 1; i++)
-            {
-                Debug.Log(worksheet.Cells[i, 1].Value.ToString() + "--" + worksheet.Cells[i, 2].Value.ToString() + "--" + worksheet.Cells[i, 3].Value.ToString());
-            }
+            //for (int i = 1; i < 3 + 1; i++)
+            //{
+            //    for (int j = 1; j < 4 + 1; j++)
+            //    {
+                    //if (worksheet.Cells[i,j].Value.ToString()=="22")
+                    //{
+                    //    print(worksheet.Cells[i,j].GetEnumerator());//返回值是B1
+                    //}
+                    //if (i == 1)
+                    //{
+                    //    print(worksheet.Cells[i, j].Value.ToString());
+                    //}
+                //}
+                //print(worksheet.GetValue(i, 1).ToString());
+            //}
+            GetValueFromId(11, worksheet);
+                
 
         }//完成一些列操作后，关闭excel文件
     }
+
+    //通过第一行的列名拿到数据
+    void GetValueFromColumn(string name,ExcelWorksheet worksheet)
+    {
+        for (int i = 1; i < 3 + 1; i++)
+        {
+            for (int j = 1; j < 4 + 1; j++)
+            {
+                if (worksheet.Cells[i, j].Value.ToString() == name)
+                {
+                    string n = worksheet.Cells[i, j].GetEnumerator().ToString();
+                    
+                }
+            }
+        }
+    }
+    //通过id拿到整行数据
+    static int num;
+    static void GetValueFromId(int id, ExcelWorksheet worksheet)
+    {
+        
+        for (int i = 1; i < 3 + 1; i++)
+        {
+            for (int j = 1; j < 4 + 1; j++)
+            {
+                if (j == 1)
+                {
+                    if (int.Parse(worksheet.Cells[i, j].Value.ToString()) == id)
+                    {
+                        string n = worksheet.Cells[i, j].GetEnumerator().ToString();
+                        num = int.Parse(n[1].ToString());
+                    }
+                }
+            }
+        }
+        for (int y = 1; y < 4 + 1; y++)
+        {
+            print(worksheet.Cells[num,y].Value.ToString());
+        }
+    }
+    //通过id和列名拿到具体某单元格的值
 
     /// <summary>
     /// 创建和删除excel文件
--- 0926FirstGame/ThreeKillGame/Assets/fight_scripts/FightCardSP.cs
+++ 0926FirstGame/ThreeKillGame/Assets/fight_scripts/FightCardSP.cs
@@ -5,36 +5,23 @@ using UnityEngine;
 
 public class FightCardSP : MonoBehaviour {
 
-    private int roundNum;   //记录当前回合数量
-    private int fightNum;   //记录当前攻击武将的位置
-    private bool isPlayerBout;  //记录是否是玩家的武将攻击回合
-
-    public GameObject[] enemyCards = new GameObject[9];//存储敌人卡牌
-    public GameObject[] playerCards = new GameObject[9];//存储己方卡牌
+    public GameObject[] enemyCards;
+    public GameObject[] playerCards;
 
     private void Awake()
     {
-        fightNum = 0;
-        roundNum = 1;
-        isPlayerBout = true;
         //敌方卡牌初始化
         for (int i = 0; i < enemyCards.Length; i++)
         {
-            if (enemyCards[i]!=null)
-            {
-                enemyCards[i].AddComponent<CardMove>();
-                //设置地方卡牌的默认先后手情况
-                enemyCards[i].GetComponent<CardMove>().IsAttack_first = ((i + 2) % 2 == 0) ? false : true;
-            }
+            enemyCards[i].AddComponent<CardMove>();
+            //设置地方卡牌的默认先后手情况
+            enemyCards[i].GetComponent<CardMove>().IsAttack_first = ((i + 2) % 2 == 0) ? false : true;
         }
         //玩家卡牌初始化
         for (int i = 0; i < enemyCards.Length; i++)
         {
-            if (playerCards[i]!=null)
-            {
-                playerCards[i].AddComponent<CardMove>();
-                playerCards[i].GetComponent<CardMove>().IsAttack_first = ((i + 2) % 2 == 0) ? true : false;
-            }
+            playerCards[i].AddComponent<CardMove>();
+            playerCards[i].GetComponent<CardMove>().IsAttack_first = ((i + 2) % 2 == 0) ? true : false;
         }
 
     }
@@ -45,91 +32,28 @@ public class FightCardSP : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        ////  该位置有武将牌                 该武将的攻击状态为备战                                                                  该武将血量不为零                                             现在是该玩家武将攻击时机
-        //if (playerCards[fightNum]!=null && playerCards[fightNum].GetComponent<CardMove>().IsAttack==StateOfAttack.ReadyForFight && playerCards[fightNum].GetComponent<CardMove>().Health > 0 && isPlayerBout)
-        
-        //回合增加
-        if (fightNum >= playerCards.Length)
+        for (int i = 0; i < playerCards.Length; i++)
         {
-            roundNum++;
-            fightNum = 0;
+            playerCards[i].GetComponent<CardMove>().Enemyindex = FindAnalogue(i);
+            playerCards[i].GetComponent<CardMove>().ChangeAttackState();
         }
-        //若有武将正在攻击
-        if (playerCards[fightNum].GetComponent<CardMove>().IsAttack != StateOfAttack.ReadyForFight || enemyCards[fightNum].GetComponent<CardMove>().IsAttack != StateOfAttack.ReadyForFight)
-            return;
-        else
-        {
-            if (isPlayerBout)
-            {
-                isPlayerBout = false;
-                if (playerCards[fightNum] != null && playerCards[fightNum].GetComponent<CardMove>().Health > 0)
-                {
-                    //先找到需要攻击的敌人
-                    playerCards[fightNum].GetComponent<CardMove>().Enemyindex = FindAnalogue(fightNum);
-                    //切换武将状态为正在攻击
-                    playerCards[fightNum].GetComponent<CardMove>().IsAttack = StateOfAttack.FightNow;
-                }
-            }
-            else
-            {
-                isPlayerBout = true;
-                if (enemyCards[fightNum] != null && enemyCards[fightNum].GetComponent<CardMove>().Health > 0)
-                {
-                    enemyCards[fightNum].GetComponent<CardMove>().Enemyindex = FindAnalogue(fightNum);
-                    enemyCards[fightNum].GetComponent<CardMove>().IsAttack = StateOfAttack.FightNow;
-                }
-                fightNum++;
-            }
-            
-        }
-        
-
 	}
 
     //找到要攻击的对手
     private GameObject FindAnalogue(int i)
     {
-        if (isPlayerBout)
+        if (enemyCards[i].GetComponent<CardMove>().Health>0)
         {
-            if (enemyCards[fightNum] != null && enemyCards[fightNum].GetComponent<CardMove>().Health>0)
-            {
-                return enemyCards[fightNum];
-            }
-            else
-            {
-                fightNum = 0;
-                while (enemyCards[fightNum].GetComponent<CardMove>().Health < 0 || enemyCards[fightNum] == null)
-                {
-                    fightNum++;
-                    if (fightNum > enemyCards.Length)
-                    {
-                        Debug.Log("玩家获胜");
-                        return null;
-                    }
-                }
-                return enemyCards[fightNum];
-            }
+            return enemyCards[i];
         }
         else
         {
-            if (playerCards[fightNum] != null && playerCards[fightNum].GetComponent<CardMove>().Health > 0)
+            i = 0;
+            while (enemyCards[i].GetComponent<CardMove>().Health < 0)
             {
-                return playerCards[fightNum];
+                i++;
             }
-            else
-            {
-                fightNum = 0;
-                while (playerCards[fightNum].GetComponent<CardMove>().Health < 0 || playerCards[fightNum] == null)
-                {
-                    fightNum++;
-                    if (fightNum > playerCards.Length)
-                    {
-                        Debug.Log("电脑获胜");
-                        return null;
-                    }
-                }
-                return playerCards[fightNum];
-            }
+            return enemyCards[i];
         }
     }
 }
