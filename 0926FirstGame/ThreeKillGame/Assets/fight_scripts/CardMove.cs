0188fb0073d175040a677e51ce05b8ebebcf38e8
MiYanLiang <5141778@qq.com>
2019-10-06 23:42:13 +0800

    战斗普通攻击基本完成，先后手和回合，等待传递武将数据

--- 0926FirstGame/ThreeKillGame/Assets/fight_scripts/CardMove.cs
+++ 0926FirstGame/ThreeKillGame/Assets/fight_scripts/CardMove.cs
@@ -1,9 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class CardMove : MonoBehaviour {
+public enum StateOfAttack
+{
+    ReadyForFight,  //0备战
+    FightNow,       //1攻击
+    FightOver       //2攻击结束
+}
 
+public class CardMove : MonoBehaviour
+{
+
+    private int moveSpeed = 1000;    //卡牌移动速度
+
+
+    private int realDamage; //造成的真实伤害
+
+    public bool isFightInThisBout; //当前回合是否进行过攻击
+
+    private int fullhealth;//满血量
     private int health; //血量
     public int Health
     {
@@ -15,20 +32,6 @@ public class CardMove : MonoBehaviour {
         set
         {
             health = value;
-        }
-    }
-
-    private int force;  //攻击力
-    public int Force
-    {
-        get
-        {
-            return force;
-        }
-
-        set
-        {
-            force = value;
         }
     }
 
@@ -46,8 +49,37 @@ public class CardMove : MonoBehaviour {
         }
     }
 
-    private int isAttack;  //攻击状态,0备战,1攻击,2攻击结束
+    private int force;  //攻击力
+    public int Force
+    {
+        get
+        {
+            return force;
+        }
 
+        set
+        {
+            force = value;
+        }
+    }
+
+    private StateOfAttack isAttack;  //记录武将的攻击状态
+    public StateOfAttack IsAttack
+    {
+        get
+        {
+            return isAttack;
+        }
+        set
+        {
+            isAttack = value;
+        }
+    }
+    //更换攻击状态
+    public void ChangeToFight(StateOfAttack state)
+    {
+        IsAttack = state;
+    }
 
     private GameObject enemyindex; //要攻击的敌人
     public GameObject Enemyindex
@@ -90,34 +122,6 @@ public class CardMove : MonoBehaviour {
         }
     }
 
-    private float critRate; //暴击率
-    public float CritRate
-    {
-        get
-        {
-            return critRate;
-        }
-
-        set
-        {
-            critRate = value;
-        }
-    }
-
-    private float critDamage;   //暴击伤害
-    public float CritDamage
-    {
-        get
-        {
-            return critDamage;
-        }
-
-        set
-        {
-            critDamage = value;
-        }
-    }
-
     private float thumpRate;    //重击率
     public float ThumpRate
     {
@@ -146,6 +150,35 @@ public class CardMove : MonoBehaviour {
         }
     }
 
+    private float critRate; //暴击率
+    public float CritRate
+    {
+        get
+        {
+            return critRate;
+        }
+
+        set
+        {
+            critRate = value;
+        }
+    }
+
+    private float critDamage;   //暴击伤害
+    public float CritDamage
+    {
+        get
+        {
+            return critDamage;
+        }
+
+        set
+        {
+            critDamage = value;
+        }
+    }
+
+
     private float armorPenetrationRate; //破甲百分比
     public float ArmorPenetrationRate
     {
@@ -160,51 +193,86 @@ public class CardMove : MonoBehaviour {
         }
     }
 
-
-    Vector2 vec = new Vector2();    //记录卡牌初始位置
+    Vector3 vec = new Vector3();    //记录卡牌初始位置
 
     private void Awake()
     {
+        isFightInThisBout = false;
         Health = 100;
-        Force = 20;
+        fullhealth = health;
+        Force = 60;
         Defence = 5;
-        isAttack = 0;
+        ChangeToFight(StateOfAttack.ReadyForFight);
         vec = gameObject.transform.position;
-    }
-
-    public void ChangeAttackState() //更换攻击状态
-    {
-        if (Health>0)
-        {
-            isAttack = 1;
-        }
-    }
-
-    private void AttackFun()    //攻击目标
-    {
-        Vector2.Lerp(gameObject.transform.position,enemyindex.transform.position,10);
-        if (gameObject.transform.position == enemyindex.transform.position)
-        {
-            isAttack = 2;
-            Debug.Log("攻击结束");
-        }
-    }
-
-    private void BackToStartPos()
-    {
-        Vector2.Lerp(gameObject.transform.position, vec, 10);
+        realDamage = force;
     }
 
     private void Update()
     {
-        if (isAttack==1 && enemyindex!=null)
+        if (IsAttack == StateOfAttack.FightNow && enemyindex != null)
         {
-            AttackFun();    //开始攻击
+            //攻击目标，武将先移动到目标身上
+            gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position, enemyindex.transform.position, moveSpeed * Time.deltaTime);
+            if (gameObject.transform.position == enemyindex.transform.position)
+            {
+                realDamage = AttackTheEnemy(force);   //得到造成的真实伤害
+
+                //敌方血条的计算和显示
+                enemyindex.GetComponent<CardMove>().Health = enemyindex.GetComponent<CardMove>().Health - realDamage;
+                if (enemyindex.GetComponent<CardMove>().Health < 0)
+                    enemyindex.GetComponent<CardMove>().Health = 0;
+                enemyindex.GetComponent<Slider>().value = ((float)enemyindex.GetComponent<CardMove>().Health) / ((float)enemyindex.GetComponent<CardMove>().fullhealth);
+            }
         }
-        if (isAttack==2)
+        if (IsAttack == StateOfAttack.FightOver)
         {
-            isAttack = 0;
-            Invoke("BackToStartPos", 0.3f); //停留一定时间后返回原位置
+            gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position, vec, moveSpeed * Time.deltaTime);
+            if (gameObject.transform.position == vec)
+            {
+                FightCardSP.isFightNow = false;
+                ChangeToFight(0);
+            }
         }
+    }
+
+    //攻击敌方武将，计算造成的战斗伤害数值
+    private int AttackTheEnemy(int force)
+    {
+        ChangeToFight(StateOfAttack.FightOver);
+        //计算是否敌方闪避
+        if (TakeSpecialAttack(enemyindex.GetComponent<CardMove>().dodgeRate))
+        {
+            Debug.Log("闪避");
+            return 0;
+        }
+        //计算是否触发重击
+        if (TakeSpecialAttack(thumpRate))
+        {
+            Debug.Log("重击");
+            force = (int)(force * thumpDamage);
+        }
+        else
+        {
+            //计算是否触发暴击
+            if (TakeSpecialAttack(critRate))
+            {
+                Debug.Log("暴击");
+                force = (int)(force * critDamage);
+            }
+        }
+
+        //添加破甲值的计算
+        return force - (int)(enemyindex.GetComponent<CardMove>().defence * (1 - armorPenetrationRate));
+    }
+
+
+    //计算是否触发特殊攻击状态
+    public bool TakeSpecialAttack(float odds)
+    {
+        int num = Random.Range(1, 101);  //随机取1-100中一个数
+        if (num <= odds * 100)
+            return true;
+        else
+            return false;
     }
 }
--- 0926FirstGame/ThreeKillGame/Assets/fight_scripts/FightCardSP.cs
+++ 0926FirstGame/ThreeKillGame/Assets/fight_scripts/FightCardSP.cs
@@ -3,57 +3,157 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FightCardSP : MonoBehaviour {
+public class FightCardSP : MonoBehaviour
+{
 
-    public GameObject[] enemyCards;
-    public GameObject[] playerCards;
+    private int roundNum;   //记录当前回合数量
+    private int fightNum;   //记录当前攻击武将的位置
+    private bool isPlayerBout;  //记录是否是玩家的武将攻击回合
+    public static bool isFightNow;  //记录现在是否正在攻击
+
+
+    public GameObject[] enemyCards = new GameObject[9];//存储敌人卡牌
+    public GameObject[] playerCards = new GameObject[9];//存储己方卡牌
 
     private void Awake()
     {
+        fightNum = 0;
+        roundNum = 1;
+        isPlayerBout = true;
+        isFightNow = false;
         //敌方卡牌初始化
         for (int i = 0; i < enemyCards.Length; i++)
         {
-            enemyCards[i].AddComponent<CardMove>();
-            //设置地方卡牌的默认先后手情况
-            enemyCards[i].GetComponent<CardMove>().IsAttack_first = ((i + 2) % 2 == 0) ? false : true;
+            if (enemyCards[i] != null)
+            {
+                enemyCards[i].AddComponent<CardMove>();
+                //设置地方卡牌的默认先后手情况
+                enemyCards[i].GetComponent<CardMove>().IsAttack_first = ((i + 2) % 2 == 0) ? false : true;
+            }
         }
         //玩家卡牌初始化
         for (int i = 0; i < enemyCards.Length; i++)
         {
-            playerCards[i].AddComponent<CardMove>();
-            playerCards[i].GetComponent<CardMove>().IsAttack_first = ((i + 2) % 2 == 0) ? true : false;
+            if (playerCards[i] != null)
+            {
+                playerCards[i].AddComponent<CardMove>();
+                playerCards[i].GetComponent<CardMove>().IsAttack_first = ((i + 2) % 2 == 0) ? true : false;
+            }
         }
 
     }
-    // Use this for initialization
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        for (int i = 0; i < playerCards.Length; i++)
-        {
-            playerCards[i].GetComponent<CardMove>().Enemyindex = FindAnalogue(i);
-            playerCards[i].GetComponent<CardMove>().ChangeAttackState();
-        }
-	}
 
+    // Update is called once per frame
+    void Update()
+    {
+        //回合增加
+        if (fightNum >= playerCards.Length)
+        {
+            roundNum++;
+            fightNum = 0;
+        }
+        //若有武将正在攻击
+        if (isFightNow)
+            return;
+
+        if (playerCards[fightNum] != null && playerCards[fightNum].GetComponent<CardMove>().IsAttack_first && playerCards[fightNum].GetComponent<CardMove>().Health > 0)
+        {
+            if (!playerCards[fightNum].GetComponent<CardMove>().isFightInThisBout)
+            {
+                isPlayerBout = true;
+                //先找到需要攻击的敌人
+                playerCards[fightNum].GetComponent<CardMove>().Enemyindex = FindAnalogue(fightNum);
+                //切换武将状态为正在攻击
+                playerCards[fightNum].GetComponent<CardMove>().IsAttack = StateOfAttack.FightNow;
+                isFightNow = true;
+                //记录当前武将在该回合已进行过攻击
+                playerCards[fightNum].GetComponent<CardMove>().isFightInThisBout = true;
+            }
+            else
+            {
+                if (enemyCards[fightNum] != null && enemyCards[fightNum].GetComponent<CardMove>().Health > 0)
+                {
+                    isPlayerBout = false;
+                    enemyCards[fightNum].GetComponent<CardMove>().Enemyindex = FindAnalogue(fightNum);
+                    //切换武将状态为正在攻击
+                    enemyCards[fightNum].GetComponent<CardMove>().IsAttack = StateOfAttack.FightNow;
+                    isFightNow = true;
+                }
+                playerCards[fightNum++].GetComponent<CardMove>().isFightInThisBout = false;
+            }
+            return;
+        }
+
+        if (enemyCards[fightNum] != null && enemyCards[fightNum].GetComponent<CardMove>().IsAttack_first && enemyCards[fightNum].GetComponent<CardMove>().Health > 0)
+        {
+            if (!enemyCards[fightNum].GetComponent<CardMove>().isFightInThisBout)
+            {
+                isPlayerBout = false;
+                enemyCards[fightNum].GetComponent<CardMove>().Enemyindex = FindAnalogue(fightNum);
+                enemyCards[fightNum].GetComponent<CardMove>().IsAttack = StateOfAttack.FightNow;
+                isFightNow = true;
+                enemyCards[fightNum].GetComponent<CardMove>().isFightInThisBout = true;
+            }
+            else
+            {
+                if (playerCards[fightNum] != null && playerCards[fightNum].GetComponent<CardMove>().Health > 0)
+                {
+                    isPlayerBout = true;
+                    playerCards[fightNum].GetComponent<CardMove>().Enemyindex = FindAnalogue(fightNum);
+                    playerCards[fightNum].GetComponent<CardMove>().IsAttack = StateOfAttack.FightNow;
+                    isFightNow = true;
+                }
+                enemyCards[fightNum++].GetComponent<CardMove>().isFightInThisBout = false;
+            }
+            return;
+        }
+
+    }
+    int selectEnemy;
     //找到要攻击的对手
     private GameObject FindAnalogue(int i)
     {
-        if (enemyCards[i].GetComponent<CardMove>().Health>0)
+        if (isPlayerBout)
         {
-            return enemyCards[i];
+            if (enemyCards[fightNum] != null && enemyCards[fightNum].GetComponent<CardMove>().Health > 0)
+            {
+                return enemyCards[fightNum];
+            }
+            else
+            {
+                selectEnemy = 0;
+                while (enemyCards[selectEnemy].GetComponent<CardMove>().Health <= 0 || enemyCards[selectEnemy] == null)
+                {
+                    selectEnemy++;
+                    if (selectEnemy > enemyCards.Length)
+                    {
+                        Debug.Log("玩家获胜");
+                        return null;
+                    }
+                }
+                return enemyCards[selectEnemy];
+            }
         }
         else
         {
-            i = 0;
-            while (enemyCards[i].GetComponent<CardMove>().Health < 0)
+            if (playerCards[fightNum] != null && playerCards[fightNum].GetComponent<CardMove>().Health > 0)
             {
-                i++;
+                return playerCards[fightNum];
             }
-            return enemyCards[i];
+            else
+            {
+                selectEnemy = 0;
+                while (playerCards[selectEnemy].GetComponent<CardMove>().Health <= 0 || playerCards[selectEnemy] == null)
+                {
+                    selectEnemy++;
+                    if (selectEnemy > playerCards.Length)
+                    {
+                        Debug.Log("电脑获胜");
+                        return null;
+                    }
+                }
+                return playerCards[selectEnemy];
+            }
         }
     }
 }
