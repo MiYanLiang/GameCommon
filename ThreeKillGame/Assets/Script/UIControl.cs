2a8010c46e989fb0e7b01da457f0e278e173929a
PoppyLiYaNan <55842087+PoppyLiYaNan@users.noreply.github.com>
2019-10-13 21:12:28 +0800

    脚本提交

---
+++ ThreeKillGame/Assets/Script/UIControl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+using OfficeOpenXml;    //引入使用EPPlus类库
+
+public class UIControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject gold;
+    public CreateAndUpdate cau;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gold.GetComponent<Text>().text = "金：" + cau.money;
+    }
+}
