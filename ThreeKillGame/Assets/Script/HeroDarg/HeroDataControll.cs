8f403b6353491dbba9c5e16246c993738202e530
PoppyLiYaNan <55842087+PoppyLiYaNan@users.noreply.github.com>
2019-10-21 23:47:33 +0800

    上信息栏提交

--- ThreeKillGame/Assets/Script/HeroDarg/HeroDataControll.cs
+++ ThreeKillGame/Assets/Script/HeroDarg/HeroDataControll.cs
@@ -9,7 +9,6 @@ public class HeroDataControll : MonoBehaviour {
     [HideInInspector]
     public List<string> HeroData = new List<string>(); //存储当前武将的数据
     List<string> heroIdDate = new List<string>();
-    int heroidtest;
     List<string> HeroDataTest = new List<string>();
     //public List<string> HeroData { get => heroData; set => heroData = value; }
     //index	    roleName	所属势力    soldierKind	  rarity  recruitingMoney  attack    defense	soldierNum	    闪避率	
@@ -22,16 +21,29 @@ public class HeroDataControll : MonoBehaviour {
     public int Grade_hero { get => grade_hero; set => grade_hero = value; }
     List<List<string>> fetterInformation = new List<List<string>>();
 
+    int heroidtest;
+    string heroName;
+    int heroKindType;
+    string heroKindName;
+    string attack;
+    string defense;
+    string soldierNum;
 
     // Use this for initialization
     void Start () {
         print(HeroData[0] + ":" + HeroData[1]);
         heroidtest = int.Parse(HeroData[0]);
-        HeroDataTest = HeroData;
-        for (int i = 0; i < HeroDataTest.Count; i++)
-        {
-            print(HeroDataTest[i]);
-        }
+        heroName = HeroData[1];
+        heroKindType = int.Parse(HeroData[3]);
+        GetHeroTypeName();
+        attack = HeroData[6];
+        defense = HeroData[7];
+        soldierNum = HeroData[8];
+        //HeroDataTest = HeroData;
+        //for (int i = 0; i < HeroDataTest.Count; i++)
+        //{
+        //    print(HeroDataTest[i]);
+        //}
     }
 
     /// <summary>
@@ -66,6 +78,11 @@ public class HeroDataControll : MonoBehaviour {
         heroIdDate.Add(heroId.ToString());
         print("点击的"+heroId);
         fetterInformation=GameObject.Find("FettrrControl").GetComponent<FetterContronl>().init_One(heroIdDate);
+        //显示点击英雄的名字  及  阶数（阶数尚未得到）
+        GameObject.Find("TopInformationBar").GetComponentsInChildren<Text>()[1].text = heroName+ "\u1500"+"1"+"阶";
+        //显示兵种及英雄相关属性  （缺少拥有个数）
+        GameObject.Find("TopInformationBar").GetComponentsInChildren<Text>()[2].text = heroKindName + "\u2000" + "拥有" + "1" + "\u2000" + "攻击" + attack + "\u2000" + "防御" + defense + "\u2000" + "士兵" + soldierNum;
+        //显示羁绊内容
         if (fetterInformation.Count > 0)
         {
             ////传递给显示详细信息
@@ -74,7 +91,7 @@ public class HeroDataControll : MonoBehaviour {
                 for (int i = 0; i < fetterInformation[j].Count; i++)
                 {
                     //print(fetterInformation[j][i]);
-                    GameObject.Find("TopInformationBar").GetComponentInChildren<Text>().text += "\u3000" + "[" + fetterInformation[j][1] + "]" + fetterInformation[j][9] + "同时上阵," + "攻击+" + fetterInformation[j][3] + "%" + "防御+" + fetterInformation[j][4] + "%" + "士兵+" + fetterInformation[j][5] + "%" + "\t";
+                    GameObject.Find("TopInformationBar").GetComponentsInChildren<Text>()[0].text += "\u3000" + "[" + fetterInformation[j][1] + "]" + fetterInformation[j][9] + "同时上阵时," + "攻击+" + fetterInformation[j][3] + "%" +","+ "防御+" + fetterInformation[j][4] + "%"+"," + "士兵+" + fetterInformation[j][5] + "%" + "\t";
                     break;
                 }
             }
@@ -84,5 +101,61 @@ public class HeroDataControll : MonoBehaviour {
             GameObject.Find("TopInformationBar").GetComponentInChildren<Text>().text = "\u3000" + "此英雄无羁绊";
         }
         heroIdDate.Clear();
+    }
+
+    //获取兵种名字
+    void GetHeroTypeName()
+    {
+        string name = "";
+        if (heroKindType == 1)
+        {
+            name = "盾兵";
+            heroKindName = name;
+        }
+        else if (heroKindType == 2)
+        {
+            name = "象兵";
+            heroKindName = name;
+        }
+        else if (heroKindType == 3)
+        {
+            name = "戟兵";
+            heroKindName = name;
+        }
+        else if (heroKindType == 4)
+        {
+            name = "禁卫";
+            heroKindName = name;
+        }
+        else if (heroKindType == 5)
+        {
+            name = "枪兵";
+            heroKindName = name;
+        }
+        else if (heroKindType == 6)
+        {
+            name = "骑兵";
+            heroKindName = name;
+        }
+        else if (heroKindType == 7)
+        {
+            name = "军师";
+            heroKindName = name;
+        }
+        else if (heroKindType == 8)
+        {
+            name = "工兵";
+            heroKindName = name;
+        }
+        else if (heroKindType == 9)
+        {
+            name = "方士";
+            heroKindName = name;
+        }
+        else if (heroKindType == 10)
+        {
+            name = "神兽";
+            heroKindName = name;
+        }
     }
 }
