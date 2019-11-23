c75e03ba5d089bf9bd01528c5392e826be24fb11
MiYanLiang <39160433+MiYanLiang@users.noreply.github.com>
2019-11-24 00:10:25 +0800

    金币多500

--- ThreeKillGame/Assets/Script/Recruit_Scripts/CreateAndUpdate.cs
+++ ThreeKillGame/Assets/Script/Recruit_Scripts/CreateAndUpdate.cs
@@ -153,6 +153,7 @@ public class CreateAndUpdate : MonoBehaviour
     {
         //money = 500;
         money = int.Parse(LoadJsonFile.difficultyChooseDatas[PlayerPrefs.GetInt("DifficultyType") - 1][3]);
+        money += 500;
         UpdateGoldOfGrade();
         SetMaxBatAndPre();  //设置最大备战位和上阵位
         //ChangeLevelText();
