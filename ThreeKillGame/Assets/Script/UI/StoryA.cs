73fb6a3fae10725892107609a4f05f981e7f0624
liuyongbin6666 <44885895+liuyongbin6666@users.noreply.github.com>
2020-01-29 16:33:48 +0800

    鸡肋玩法UI

---
+++ ThreeKillGame/Assets/Script/UI/StoryA.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StoryA : MonoBehaviour
+{
+    [SerializeField]
+    Transform StoryAObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    //初始化
+    public void InitializeStory()
+    {
+        int storyId = Random.Range(0,LoadJsonFile.StoryATableDates.Count);
+
+        //故事标题
+        StoryAObject.GetChild(2).GetComponent<Text>().text = LoadJsonFile.StoryATableDates[storyId][2];
+        //故事主体
+        StoryAObject.GetChild(3).GetComponent<Text>().text = LoadJsonFile.StoryATableDates[storyId][3];
+        //选项
+        for (int i=4; i<6; i++)
+        {
+            StoryAObject.GetChild(i).GetChild(0).GetComponent<Text>().text = LoadJsonFile.StoryATableDates[storyId][i+1];
+        }
+        //exit
+        StoryAObject.GetChild(6).GetChild(0).GetComponent<Text>().text = LoadJsonFile.StoryATableDates[storyId][4];
+    }
+    //玩家做出选择
+    public void SelectOption()
+    {
+
+    }
+}
